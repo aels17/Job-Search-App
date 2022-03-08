ab0e5f1b0018eda6855299936b92e05883b277ea
Evan Chernicky <echernicky@gmail.com>
2022-03-08 18:57:58 -0500

    removed 'var' from date

--- src/Components/CreateJob.js
+++ src/Components/CreateJob.js
@@ -12,10 +12,10 @@ function CreateJob({createJob}) {
 
 
   //Get Today's Date and auto fill in date
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = today.getFullYear();
   
   today = yyyy + '-' + mm + '-' + dd;
 
