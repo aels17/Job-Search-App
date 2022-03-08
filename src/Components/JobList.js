f46d9f2f73ae616e15b9f589aa9a8f06f4671859
Evan Chernicky <echernicky@gmail.com>
2022-03-08 12:23:22 -0500

    Added some more stuff

--- src/Components/JobList.js
+++ src/Components/JobList.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import JobCard from './JobCard'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 
 
 function JobList() {
   return (
-        <div>
-            Hello from JobLIst
-        </div>
+    <div>
+        <Row className="d-flex justify-content-end p-2">
+            <Col>
+                <JobCard />      
+            </Col>
+            <Col>
+            <JobCard />             
+            </Col>
+            <Col>
+            <JobCard />             
+            </Col>
+            <Col>
+            <JobCard />             
+            </Col>
+        </Row>
+
+
+
+    </div>
   );
 }
 
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
-
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
 
 function SearchBar() {
   return (
-    <div>
-        test
-    </div>
+        <Form>
+            <Row className="align-items-center">
+                <Col sm={3} className="my-1">
+                <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
+                    Name
+                </Form.Label>
+                <Form.Control id="inlineFormInputName" placeholder="Jane Doe" />
+                </Col>
+                <Col xs="auto" className="my-1">
+                <Button type="submit">Submit</Button>
+                </Col>
+            </Row>
+        </Form>    
   );
 }
 
