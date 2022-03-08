b88c28eb443d85d56b4a473a328a29392790db50
Evan Chernicky <echernicky@gmail.com>
2022-03-08 12:03:58 -0500

    added routing

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import Navbar from './Navbar';
 import Home from './Home'
+import CreateJob from './CreateJob'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Switch>
+        <Route exact path="/create-job">
+          <CreateJob />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/CreateJob.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateJob() {
+  return (
+    <div>
+        Hello from create job
+    </div>
+  );
+}
+
+export default CreateJob;
