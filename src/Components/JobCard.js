9de666114b52ce9b4394db4465df4868abccf8e5
Evan Chernicky <echernicky@gmail.com>
2022-03-09 17:07:21 -0500

    added css styling to cards

--- src/Components/JobCard.js
+++ src/Components/JobCard.js
@@ -10,7 +10,7 @@ function JobCard({job}) {
 
   return (
     <Card className="m-auto  h-100" >
-    <Card.Header  style={{fontWeight: "700", fontSize: "14px", paddingLeft: "1.5rem"}}>{status}</Card.Header>
+    <Card.Header  className={status.toLowerCase().replace(' ', '-')} style={{fontWeight: "700", fontSize: "14px", paddingLeft: "1.5rem"}}>{status}</Card.Header>
     <Card.Body className="p-4">
       <Card.Title>{company}</Card.Title>
       <Card.Subtitle className="mb-2 text-muted pb-2">{job_title}</Card.Subtitle>
