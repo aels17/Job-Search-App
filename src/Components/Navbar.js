1d388be7a0bed97872df4740c0576cdfa54f72c2
Evan Chernicky <echernicky@gmail.com>
2022-03-10 15:27:07 -0500

    added conditional nav button to go home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
@@ -7,11 +7,25 @@ import Button from 'react-bootstrap/Button'
 
 
 function NavBar() {
+
+    const location = useLocation()
+    let rowReverse = ""
+
+    //Add Go Home Button if not on the homepage
+    function addBackButton() {
+        if (location.pathname !== '/') {
+
+            return <Link to="/" ><Button variant="secondary"> &larr; Back Home</Button>{' '}</Link>
+        }
+    }
+
+
   return (
       <header>
             <Navbar bg="light" expand="lg">
-                <Container className="d-flex justify-content-end p-2">
+                <Container className={"d-flex justify-content-space-between p-2 flex-row-reverse"}>
                     <Link to="/create-job" ><Button variant="primary">Create a New Job</Button>{' '}</Link>
+                    {addBackButton()}
                 </Container>
             </Navbar>
         </header>
